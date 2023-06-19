c4423be058dcb1aeeb56db11f0cf1c5caed7f70c
chanjieru <chanjieru2003@gmail.com>
2023-06-19 14:40:23 +0800

    First commit

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='bg-slate-700 font-sans text-3xl text-slate-300 text-center pt-20 pb-40'>SKYSTALKER</div>
+  )
+}
+
+export default App
