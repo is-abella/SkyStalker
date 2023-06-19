66e484164a20dd0af237d4c62c30cbbdee19a743
chanjieru <chanjieru2003@gmail.com>
2023-06-19 21:19:10 +0800

    Add components and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/Home.jsx";
 import './App.css'
 
 function App() {
   return (
-    <div className='bg-slate-700 font-sans text-3xl text-slate-300 text-center pt-20 pb-40'>SKYSTALKER</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div class="mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl text-center pt-10">
+        Skystalker
+    </div>
+  )
+}
+
+export default Header
