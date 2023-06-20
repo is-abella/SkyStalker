f3e76ae8b9faefa0be944a508792d3994e822d5e
chanjieru <chanjieru2003@gmail.com>
2023-06-20 19:48:05 +0800

    Add search feature for 'to location'

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Header from '../components/Header'
 import SearchFrom from '../components/SearchFrom'
+import SearchTo from '../components/SearchTo'
 import AirportData from '../airports.json'
 
 function Home() {
   return (
     <div>
         <Header/>
-        <SearchFrom placeholder="Search for a city..." data={AirportData}/>
+        <div className='flex gap-5 justify-center'>
+          <SearchFrom placeholder="Search for a city..." data={AirportData}/>
+          <SearchTo placeholder="Search for a city..." data={AirportData}/>
+        </div>
     </div>
 
   )
