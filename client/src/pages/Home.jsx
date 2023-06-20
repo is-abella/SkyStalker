bf818c79151549c75086ed579143d6bf53a00f62
chanjieru <chanjieru2003@gmail.com>
2023-06-20 14:39:56 +0800

    Add SearchFrom feature

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Header from '../components/Header'
+import SearchFrom from '../components/SearchFrom'
+import AirportData from '../airports.json'
 
-const Home = () => {
+function Home() {
   return (
     <div>
         <Header/>
+        <SearchFrom placeholder="Search for a city..." data={AirportData}/>
     </div>
 
   )
