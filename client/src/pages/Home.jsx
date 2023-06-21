96d062667c2df2e9e82fea434f33efd62a116745
chanjieru <chanjieru2003@gmail.com>
2023-06-22 00:05:45 +0800

    Add Travellers and CabinClass features

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,16 +5,20 @@ import SearchTo from "../components/SearchTo";
 import AirportData from "../airports.json";
 import DepartDate from "../components/DepartDate";
 import ReturnDate from "../components/ReturnDate";
+import CabinClass from "../components/CabinClass";
+import Travellers from "../components/Travellers";
 
 function Home() {
 	return (
 		<div>
 			<Header />
-			<div className="flex gap-5 justify-center">
+			<div className="flex gap-2 justify-center">
 				<SearchFrom placeholder="Country/city" data={AirportData} />
 				<SearchTo placeholder="Country/city" data={AirportData} />
 				<DepartDate />
 				<ReturnDate />
+				<Travellers />
+				<CabinClass />
 			</div>
 		</div>
 	);
