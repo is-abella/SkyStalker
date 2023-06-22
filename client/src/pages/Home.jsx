38ff787d9a5d0b86bb83ac93bb3823b479cf662d
chanjieru <chanjieru2003@gmail.com>
2023-06-22 17:49:56 +0800

    Add search button + navigation, fromInput, toInput
    Clicking search button navigates to Results page with input values.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
 import SearchFrom from "../components/SearchFrom";
 import SearchTo from "../components/SearchTo";
@@ -7,18 +7,32 @@ import DepartDate from "../components/DepartDate";
 import ReturnDate from "../components/ReturnDate";
 import CabinClass from "../components/CabinClass";
 import Travellers from "../components/Travellers";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+	const navigate = useNavigate();
+	const [fromInput, setFromInput] = useState("");
+	const [toInput, setToInput] = useState("");
+
+	const handleSearch = () => {
+		navigate("/results", { state: { fromInput: fromInput, toInput: toInput } });
+	};
+
 	return (
 		<div>
 			<Header />
 			<div className="flex gap-2 justify-center">
-				<SearchFrom placeholder="Country/city" data={AirportData} />
-				<SearchTo placeholder="Country/city" data={AirportData} />
+				<SearchFrom
+					placeholder="Country/city"
+					data={AirportData}
+					setFromInput={setFromInput}
+				/>
+				<SearchTo placeholder="Country/city" data={AirportData} setToInput={setToInput} />
 				<DepartDate />
 				<ReturnDate />
 				<Travellers />
 				<CabinClass />
+				<button onClick={handleSearch}>Search</button>
 			</div>
 		</div>
 	);
