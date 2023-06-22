9c62ff52077b8c96c3b3cce5289ae3ff55cef048
chanjieru <chanjieru2003@gmail.com>
2023-06-22 17:46:21 +0800

    Add route to Results page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,17 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
-import './App.css'
+import Results from "./pages/Results.jsx";
+import "./App.css";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-      </Routes>
-    </BrowserRouter>
-  )
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/results" element={<Results />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;
