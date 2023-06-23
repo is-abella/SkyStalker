626342909508924ebd5f5a0dc0f7f41f6e9fb8c5
chanjieru <chanjieru2003@gmail.com>
2023-06-23 16:59:12 +0800

    Add states from Home

--- client/src/pages/Results.jsx
+++ client/src/pages/Results.jsx
@@ -5,12 +5,28 @@ function Results() {
 	const location = useLocation();
 	const fromInput = location.state?.fromInput || "";
 	const toInput = location.state?.toInput || "";
+	const departDate = location.state?.departDate || new Date();
+	const formatDepartDate = `${departDate.getDate()}/${
+		departDate.getMonth() + 1
+	}/${departDate.getFullYear()}`;
+	const returnDate = location.state?.returnDate || new Date();
+	const formatReturnDate = `${returnDate.getDate()}/${
+		returnDate.getMonth() + 1
+	}/${returnDate.getFullYear()}`;
+	const adults = location.state?.adults || 1;
+	const children = location.state?.children || 0;
+	const cabinClass = location.state?.cabinClass || "Economy";
 
 	return (
 		<div>
 			<h1>Results</h1>
 			<div>From: {fromInput}</div>
 			<div>To: {toInput}</div>
+			<div>Depart: {formatDepartDate}</div>
+			<div>Return: {formatReturnDate}</div>
+			<div>Adults: {adults}</div>
+			<div>Children: {children}</div>
+			<div>Cabin class: {cabinClass}</div>
 		</div>
 	);
 }
