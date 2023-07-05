0efc95a6f60731670684cacc0c3cc73205f603d7
chanjieru <chanjieru2003@gmail.com>
2023-07-05 13:20:19 +0800

    Add backend server and webscrapping function

---
+++ api/index.js
@@ -0,0 +1,27 @@
+//this is the main file?
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import { findCheapestFlights } from "./webscrapping.js";
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/search", (req, res) => {
+	const userInput = req.body; // Assuming the user inputs are sent in the request body
+	// Call the findCheapestFlights function with the user inputs
+	findCheapestFlights(userInput)
+		.then((flightData) => {
+			// Send the flight data as the API response
+			res.json(flightData);
+		})
+		.catch((error) => {
+			// Handle any errors that occurred during the search
+			res.status(500).json({ error: "An error occurred during the flight search" });
+		});
+});
+
+app.listen(3000, () => {
+	console.log("Server is running on port 3000");
+});
