3267f0b06a0098fed4292067fda863a068dbdc54
chanjieru <chanjieru2003@gmail.com>
2023-07-12 12:39:14 +0800

    Use Express's middleware to parse JSON

--- api/index.js
+++ api/index.js
@@ -1,25 +1,29 @@
 //this is the main file?
 import express from "express";
-import bodyParser from "body-parser";
 import cors from "cors";
 import { findCheapestFlights } from "./searchFlights.js";
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 
 app.post("/search", (req, res) => {
-	const userInput = req.body; // Assuming the user inputs are sent in the request body
-	// Call the findCheapestFlights function with the user inputs
-	findCheapestFlights(userInput)
-		.then((flightData) => {
-			// Send the flight data as the API response
-			res.json(flightData);
-		})
-		.catch((error) => {
-			// Handle any errors that occurred during the search
-			res.status(500).json({ error: "An error occurred during the flight search" });
-		});
+	try {
+		const userInput = req.body;
+		console.log("index.js:", userInput);
+		findCheapestFlights(userInput)
+			.then((flightData) => {
+				res.json(flightData);
+				console.log("findcheapflights ran");
+			})
+			.catch((error) => {
+				console.error(error);
+				res.status(500).json({ error: "An error occurred during the flight search" });
+			});
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: "An error occurred during the flight search" });
+	}
 });
 
 app.listen(3000, () => {
