2e22ce2c8964d98bd427ca2752bc59291d0c04e6
chanjieru <chanjieru2003@gmail.com>
2023-07-26 18:52:37 +0800

    Add additional classes

--- client/src/pages/htmlConverter.js
+++ client/src/pages/htmlConverter.js
@@ -4,73 +4,75 @@ function htmlConverter(data, fromInput, toInput, tripWay, travellers, cabinClass
 	if (tripWay === "One-way") {
 		let htmlString = `
     <div class="results-main">
-    <h2>Results</h2>
     ${data
 		.map((flight) => {
 			const price = parseFloat(flight.price.replace(/[^0-9.-]+/g, ""));
 			return `
         <div class="option">
           <div class="left">
-            <div class="flight">
+            <div class="going">
               <div class="details">
-                <div>${flight.flightTiming}</div>
+                <div class="time">${flight.flightTiming}</div>
                 <div>${fromInput} - ${toInput}</div>
               </div>
-              <div>${flight.stops}</div>
-              <div>${flight.flightDuration}</div>
+              <div class="stops">${flight.stops}</div>
+              <div class="duration">${flight.flightDuration}</div>
             </div>
             <div>${flight.airlines}</div>
           </div>
           <div class="right">
-            <div>${travellers > 1 ? `${flight.price}/person` : `${flight.price}`}</div>
-            <div>${travellers > 1 ? `S$ ${price * travellers} total` : ""}</div>
+            <div class="price">${
+				travellers > 1 ? `${flight.price} / person` : `${flight.price}`
+			}</div>
+            <div class="price">${travellers > 1 ? `S$ ${price * travellers} total` : ""}</div>
             <div>${cabinClass}</div>           
           </div>
         </div>`;
 		})
 		.join("")}
     </div>`;
-    return htmlString;
+		return htmlString;
 	} else {
-    let htmlString = `
+		let htmlString = `
       <div class="results-main">
-        <h2>Results</h2>
         ${data
-        .map((flight) => {
-          const price = parseFloat(flight.price.replace(/[^0-9.-]+/g, ""));
-          return `
+			.map((flight) => {
+				const price = parseFloat(flight.price.replace(/[^0-9.-]+/g, ""));
+				return `
               <div class="option">
                 <div class="left">
                   <div class="going">
                     <div class="details">
-                      <div>${flight.goingToDetails.flightTiming}</div>
+                      <div class="time">${flight.goingToDetails.flightTiming}</div>
                       <div>${fromInput} - ${toInput}</div>
                     </div>
-                    <div>${flight.goingToDetails.stops}</div>
-                    <div>${flight.goingToDetails.flightDuration}</div>
+                    <div class="stops">${flight.goingToDetails.stops}</div>
+                    <div class="duration">${flight.goingToDetails.flightDuration}</div>
                   </div>
                   <div class="return">
                     <div class="details">
-                      <div>${flight.comingBackDetails.flightTiming}</div>
+                      <div class="time">${flight.comingBackDetails.flightTiming}</div>
                       <div>${toInput} - ${fromInput}</div>
                     </div>
-                    <div>${flight.comingBackDetails.stops}</div>
-                    <div>${flight.comingBackDetails.flightDuration}</div>
+                    <div class="stops">${flight.comingBackDetails.stops}</div>
+                    <div class="duration">${flight.comingBackDetails.flightDuration}</div>
                   </div>
                   <div class="airline">${flight.airlines}</div>
                 </div>
                 <div class="right">
-                  <div>${travellers > 1 ? `${flight.price}/person` : `${flight.price}`}</div>
-                  <div>${travellers > 1 ? `S$ ${price * travellers} total` : ""}</div>
+                  <div class="price">${
+						travellers > 1 ? `${flight.price} / person` : `${flight.price}`
+					}</div>
+                  <div class="price">${travellers > 1 ? `S$ ${price * travellers} total` : ""}</div>
                   <div>${cabinClass}</div>
                 </div>
               </div>
             `;
-        })
-        .join("")}
+			})
+			.join("")}
       </div>`;
-    return htmlString;
-  }
+		return htmlString;
+	}
 }
 
 export default htmlConverter;
