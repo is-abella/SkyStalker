85c2b64cc44fc4ce6a550bfd908897e1df1ffb08
chanjieru <chanjieru2003@gmail.com>
2023-08-01 00:38:27 +0800

    Update handleTrackFlights function
    Make a post request to the /send-email endpoint with user inputs and email
    address.

--- client/src/components/Email.jsx
+++ client/src/components/Email.jsx
@@ -1,29 +1,66 @@
 import React, { useState } from "react";
 import "./Email.css";
 
-function Email() {
+function Email({ flightInfo }) {
 	const [email, setEmail] = useState("");
+	const [submitted, setSubmitted] = useState(false);
 
 	const handleEmailChange = (event) => {
 		setEmail(event.target.value);
 	};
 
-	const handleTrackFlights = () => {};
+	const handleTrackFlights = () => {
+		const flightData = {
+			...flightInfo,
+			email,
+		};
+
+		fetch("http://localhost:3000/send-email", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(flightData),
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				if (data.success) {
+					// Email sent successfully, do something here (e.g., show a success message)
+					console.log("Email sent successfully");
+				} else {
+					// Some error occurred during email sending, handle it here
+					console.error("Error sending email");
+				}
+			})
+			.catch((error) => {
+				console.error("Error sending email:", error);
+				// Handle error if email sending fails
+			});
+
+		setSubmitted(true);
+	};
 
 	return (
 		<div className="email">
-			<div>
-				Receive email alerts for this trip. We'll let you know if prices have changed.
-			</div>
-			<input
-				type="email"
-				value={email}
-				onChange={handleEmailChange}
-				placeholder="Enter your email address"
-			/>
-			<button className="search" onClick={handleTrackFlights}>
-				TRACK FLIGHTS
-			</button>
+			{!submitted ? (
+				<>
+					<div>
+						Receive email alerts for this trip and we'll let you know if prices have
+						changed.
+					</div>
+					<input
+						type="email"
+						value={email}
+						onChange={handleEmailChange}
+						placeholder="Enter your email address"
+					/>
+					<button className="search" onClick={handleTrackFlights}>
+						TRACK FLIGHTS
+					</button>
+				</>
+			) : (
+				<div>Check your inbox for updates soon!</div>
+			)}
 		</div>
 	);
 }
